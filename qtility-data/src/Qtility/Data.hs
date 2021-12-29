306fdcd0035bcbcb80604dd3515cb8616a8d8801
Rickard Andersson <gonz@severnatazvezda.com>
2021-12-29 17:45:49 +0000

    add `fromMaybeM` & `findM` to `Qtility.Data`
    `fromMaybeM` is just a version of `fromEitherM` that allows us to turn
    the `Either` into a `Maybe` and throw it, so it's the combination of
    `note` & `fromEitherM`.
    
    `findM` is just a version of `find` lifted into `m`

--- qtility-data/src/Qtility/Data.hs
+++ qtility-data/src/Qtility/Data.hs
@@ -14,3 +14,15 @@ hush = either (const Nothing) Just
 -- | 'readMaybe' but for 'Text'.
 tReadMaybe :: (Read a) => Text -> Maybe a
 tReadMaybe = Text.unpack >>> readMaybe
+
+-- | Takes a monadic action producing a @m Maybe@ and throws a given exception @e@ from it. This
+-- is just a specialization of @'fromEitherM' '$' note e '<$>' action@.
+fromMaybeM :: (Exception e, MonadIO m) => e -> m (Maybe a) -> m a
+fromMaybeM e = fmap (note e) >>> fromEitherM
+
+-- | Lifted version of 'Data.List.find'.
+findM :: (Monad m) => (a -> m Bool) -> [a] -> m (Maybe a)
+findM _p [] = pure Nothing
+findM p (a : as) = do
+  result <- p a
+  if result then pure (Just a) else findM p as
