f887523805d696f78d334c7faa2fcccd29a8a8f8
Rickard Andersson <gonz@severnatazvezda.com>
2022-01-01 11:12:34 +0000

    add documentation for `deriveLensAndJSON`

--- qtility-th/src/Qtility/TH.hs
+++ qtility-th/src/Qtility/TH.hs
@@ -5,6 +5,16 @@ import Language.Haskell.TH
 import Qtility.TH.JSON (deriveJSON)
 import RIO
 
+-- | Derives both lens definitions as well as both 'ToJSON' and 'FromJSON'. The type is assumed to
+-- follow the format that 'deriveJSON' requires, where all fields are prefixed with an underscore as
+-- well as the name of the type:
+--
+-- @
+--     data Foo = Foo { _fooBar :: Int, _fooBaz :: String }
+-- @
+--
+-- This will generate @fooBar@ and @fooBaz@ lenses and 'ToJSON' and 'FromJSON' instances for the
+-- structure without the prefix at all, i.e. @bar@ and @baz@ fields.
 deriveLensAndJSON :: Name -> Q [Dec]
 deriveLensAndJSON name = do
   lenses <- makeLenses name
