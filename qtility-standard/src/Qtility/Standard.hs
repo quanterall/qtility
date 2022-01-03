dd240e43a673e28849a3b49174eb693449281b6a
Rickard Andersson <gonz@severnatazvezda.com>
2022-01-03 14:02:55 +0000

    qtility-standard: add `Qtility.TH` to exports

--- qtility-standard/src/Qtility/Standard.hs
+++ qtility-standard/src/Qtility/Standard.hs
@@ -20,6 +20,7 @@ import qualified Data.Aeson as Aeson
 import Qtility.Data as Export
 import Qtility.Environment as Export
 import Qtility.Environment.Types as Export
+import Qtility.TH as Export
 import Qtility.TH.JSON as Export
 import Qtility.TH.Lens as Export
 import RIO as Export hiding (fromEither, fromEitherM)
