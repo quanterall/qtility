e81e575d9fb665209a340a738cdf9695bb6caee3
Rickard Andersson <gonz@severnatazvezda.com>
2022-01-03 13:53:55 +0000

    qtility-standard: add `Data.Aeson`
    Adds the usual niceties from `Data.Aeson` as well as an alias for
    `Options` called `AesonOptions`, since it's such an overloaded name.

--- qtility-standard/src/Qtility/Standard.hs
+++ qtility-standard/src/Qtility/Standard.hs
@@ -1,8 +1,24 @@
-module Qtility.Standard (module Export) where
+module Qtility.Standard
+  ( module Export,
+    AesonOptions,
+  )
+where
 
+import Data.Aeson as Export
+  ( FromJSON (..),
+    ToJSON (..),
+    eitherDecode,
+    eitherDecode',
+    eitherDecodeStrict,
+    eitherDecodeStrict',
+    encode,
+  )
+import qualified Data.Aeson as Aeson
 import Qtility.Data as Export
 import Qtility.Environment as Export
 import Qtility.Environment.Types as Export
 import Qtility.TH.JSON as Export
 import Qtility.TH.Lens as Export
 import RIO as Export hiding (fromEither, fromEitherM)
+
+type AesonOptions = Aeson.Options
