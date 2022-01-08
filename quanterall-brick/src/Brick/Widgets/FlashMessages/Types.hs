d888be4224484c6b6c60346d81449f45feb77231
Rickard Andersson <gonz@severnatazvezda.com>
2022-01-08 12:32:56 +0000

    quanterall-brick: add doc comments to flash message types

--- quanterall-brick/src/Brick/Widgets/FlashMessages/Types.hs
+++ quanterall-brick/src/Brick/Widgets/FlashMessages/Types.hs
@@ -5,14 +5,19 @@ module Brick.Widgets.FlashMessages.Types where
 import Control.Lens.TH (makeClassyPrisms)
 import RIO
 
+-- | Flash message content, a message meant to inform the user of an application of a recent event
+-- either succeeding or failing, or otherwise interesting information.
 data FlashMessage
   = FlashSuccess !Text
   | FlashError !Text
   deriving (Eq, Show)
 
+-- | Represents an event to modify the application's flash message state.
 data FlashMessageEvent
-  = RemoveFlashMessage !Int
-  | AddFlashMessage !FlashMessage
+  = -- | Removes a flash message, addressed by its index/id.
+    RemoveFlashMessage !Int
+  | -- | Adds a flash message.
+    AddFlashMessage !FlashMessage
   deriving (Eq, Show)
 
 foldMapM makeClassyPrisms [''FlashMessage]
