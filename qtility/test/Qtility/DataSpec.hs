d2282a6cab126975fb7b8d012b6b6e9449766972
Rickard Andersson <gonz@severnatazvezda.com>
2022-01-17 20:52:29 +0000

    qtility: switch to `QuickCheck` for prop tests

--- qtility/test/Qtility/DataSpec.hs
+++ qtility/test/Qtility/DataSpec.hs
@@ -2,30 +2,28 @@
 
 module Qtility.DataSpec where
 
-import qualified Hedgehog.Gen as Gen
 import Qtility.Data
 import RIO
 import Test.Hspec
-import Test.Hspec.Hedgehog
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
 
 spec :: Spec
 spec = do
   describe "`note`" $ do
-    it "Should annotate `Just` & `Nothing` correctly" $ do
-      hedgehog $ do
-        x <- forAll Gen.enumBounded
+    prop "Should annotate `Just` & `Nothing` correctly" $ do
+      quickCheck $ \x -> do
         let hasValue = note @String @Int "x" (Just x)
             doesNotHaveValue = note @String @Int "we have nothing" Nothing
-        hasValue === Right x
-        doesNotHaveValue === Left "we have nothing"
+        hasValue `shouldBe` Right x
+        doesNotHaveValue `shouldBe` Left "we have nothing"
   describe "`hush`" $ do
-    it "Should silence `Left`s and keep `Right`s" $ do
-      hedgehog $ do
-        x <- forAll Gen.enumBounded
+    prop "Should silence `Left`s and keep `Right`s" $ do
+      quickCheck $ \x -> do
         let hasValue = hush @String @Int $ Right x
             doesNotHaveValue = hush @String @Int $ Left "we have nothing"
-        hasValue === Just x
-        doesNotHaveValue === Nothing
+        hasValue `shouldBe` Just x
+        doesNotHaveValue `shouldBe` Nothing
   describe "tReadMaybe" $ do
     it "Parses float values" $ do
       tReadMaybe @Int "1" `shouldBe` Just 1
@@ -51,14 +49,13 @@ spec = do
       tReadMaybe @Float "1.0e-2" `shouldBe` Just 0.01
       tReadMaybe @Float "1.0e+2" `shouldBe` Just 100.0
       tReadMaybe @Float "1.0e2" `shouldBe` Just 100.0
-    it "Should be able to parse random integer values in containers" $ do
-      hedgehog $ do
-        x <- forAll Gen.enumBounded
+    prop "Should be able to parse random integer values in containers" $ do
+      quickCheck $ \x -> do
         let just = "Just " <> tshow @Int x
             nothing = "Nothing"
             right = "Right " <> tshow @Int x
             left = "Left " <> tshow @String "we have nothing"
-        tReadMaybe @(Maybe Int) just === Just (Just x)
-        tReadMaybe @(Maybe Int) nothing === Just Nothing
-        tReadMaybe @(Either String Int) right === Just (Right x)
-        tReadMaybe @(Either String Int) left === Just (Left "we have nothing")
+        tReadMaybe @(Maybe Int) just `shouldBe` Just (Just x)
+        tReadMaybe @(Maybe Int) nothing `shouldBe` Just Nothing
+        tReadMaybe @(Either String Int) right `shouldBe` Just (Right x)
+        tReadMaybe @(Either String Int) left `shouldBe` Just (Left "we have nothing")
