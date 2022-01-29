f551c230d649746efdffabb0170fa8c53f7c5d98
Rickard Andersson <gonz@severnatazvezda.com>
2022-01-30 00:29:35 +0200

    qtility: make exports explicit module names

--- qtility/src/Qtility.hs
+++ qtility/src/Qtility.hs
@@ -1,18 +1,38 @@
 module Qtility
-  ( module Export,
+  ( module Control.Concurrent.STM.TBMChan,
+    module Control.Concurrent.STM.TBMQueue,
+    module Control.Exception.Lens,
+    module Control.Lens.Prism,
+    module Qtility.Aeson.Lenses,
+    module Qtility.Aliases,
+    module Qtility.Aliases.IOErrorLens,
+    module Qtility.TH.JSON,
+    module Qtility.TH,
+    module Qtility.Exceptions,
+    module Qtility.Environment.Types,
+    module Qtility.Environment,
+    module Qtility.Data,
+    module System.IO.Error.Lens,
+    module RIO,
+    module Control.Exception.Safe,
+    module Control.Lens,
+    module Control.Lens.Combinators,
+    module Control.Lens.Operators,
+    module Control.Lens.TH,
+    module Data.Aeson,
   )
 where
 
-import Control.Concurrent.STM.TBMChan as Export
-import Control.Concurrent.STM.TBMQueue as Export
-import Control.Exception.Lens as Export
-import Control.Exception.Safe as Export (Handler (..), catches, catchesDeep)
-import Control.Lens as Export (re, (#))
-import Control.Lens.Combinators as Export (at, ix, non)
-import Control.Lens.Operators as Export ((?~))
-import Control.Lens.Prism as Export
-import Control.Lens.TH as Export (makeClassy, makeClassyPrisms, makeLenses, makeWrapped)
-import Data.Aeson as Export
+import Control.Concurrent.STM.TBMChan
+import Control.Concurrent.STM.TBMQueue
+import Control.Exception.Lens
+import Control.Exception.Safe (Handler (..), catches, catchesDeep)
+import Control.Lens (re, (#))
+import Control.Lens.Combinators (at, ix, non)
+import Control.Lens.Operators ((?~))
+import Control.Lens.Prism
+import Control.Lens.TH (makeClassy, makeClassyPrisms, makeLenses, makeWrapped)
+import Data.Aeson
   ( FromJSON (..),
     SumEncoding (..),
     ToJSON (..),
@@ -31,17 +51,17 @@ import Data.Aeson as Export
     (.:?),
     (.=),
   )
-import Qtility.Aeson.Lenses as Export
-import Qtility.Aliases as Export
-import Qtility.Aliases.IOErrorLens as Export
-import Qtility.Data as Export
-import Qtility.Environment as Export
-import Qtility.Environment.Types as Export
-import Qtility.Exceptions as Export
-import Qtility.TH as Export
-import Qtility.TH.JSON as Export
-import RIO as Export hiding (Handler, catches, catchesDeep, fromEither, fromEitherM)
-import System.IO.Error.Lens as Export hiding
+import Qtility.Aeson.Lenses
+import Qtility.Aliases
+import Qtility.Aliases.IOErrorLens
+import Qtility.Data
+import Qtility.Environment
+import Qtility.Environment.Types
+import Qtility.Exceptions
+import Qtility.TH
+import Qtility.TH.JSON
+import RIO hiding (Handler, catches, catchesDeep, fromEither, fromEitherM)
+import System.IO.Error.Lens hiding
   ( description,
     errno,
     errorType,
