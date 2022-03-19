5819da15673c78912be0d2506121ebac533847c5
Rickard Andersson <gonz@severnatazvezda.com>
2022-03-19 11:23:52 +0200

    qtility: add `runMasterDB` & `HasPostgresqlMasterPool`
    For running queries against the `postgres` database

--- qtility/src/Database/PostgreSQL/Simple/Utilities.hs
+++ qtility/src/Database/PostgreSQL/Simple/Utilities.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Database.PostgreSQL.Simple.Utilities where
 
 import Data.Pool (Pool, withResource)
-import Database.PostgreSQL.Simple (Connection)
+import Database.PostgreSQL.Simple (Connection, Only (..), query)
+import Database.PostgreSQL.Simple.SqlQQ (sql)
 import Qtility
 
 class HasPostgresqlPool env where
@@ -10,6 +13,15 @@ class HasPostgresqlPool env where
 instance HasPostgresqlPool (Pool Connection) where
   postgresqlPoolL = id
 
+class HasPostgresqlMasterPool env where
+  postgresqlMasterPoolL :: Lens' env (Pool Connection)
+
+instance HasPostgresqlMasterPool (Pool Connection) where
+  postgresqlMasterPoolL = id
+
+-- | Runs an action against the connection pool that has been chosen for the main pool of the
+-- application. This is different to 'runMasterDB', which (idiomatically) would run against the
+-- @postgres@ database.
 runDB ::
   (MonadReader env m, MonadUnliftIO m, HasPostgresqlPool env) =>
   (Connection -> IO a) ->
@@ -17,3 +29,20 @@ runDB ::
 runDB action = do
   pool <- view postgresqlPoolL
   liftIO $ withResource pool action
+
+-- | Runs an action against the master database of PostgreSQL, i.e. the @postgres@ database.
+runMasterDB ::
+  (MonadIO m, MonadReader env m, HasPostgresqlMasterPool env) =>
+  (Connection -> IO a) ->
+  m a
+runMasterDB action = do
+  pool <- view postgresqlMasterPoolL
+  liftIO $ withResource pool action
+
+doesDatabaseExist :: Text -> Connection -> IO Bool
+doesDatabaseExist name connection = do
+  (length @[] @[Text] >>> (> 0))
+    <$> query
+      connection
+      [sql|SELECT datname FROM pg_catalog.pg_database WHERE datname = ?|]
+      (Only name)
