ad84c86f7c8d2f8f1d53908a906f9bdb3708c36c
Rickard Andersson <gonz@severnatazvezda.com>
2022-06-05 22:18:50 +0300

    qtility: add doc comment to `withJsonFile`

--- qtility/src/Qtility/File.hs
+++ qtility/src/Qtility/File.hs
@@ -7,6 +7,8 @@ import Qtility.File.Types
 import RIO hiding (fromEither)
 import qualified RIO.ByteString as ByteString
 
+-- | Reads a file and attempts to decode the contents as a JSON value, throwing a
+-- 'JsonFileDecodingError' if it cannot do so, then runs the given closure on the value.
 withJsonFile :: (MonadIO m, MonadThrow m) => FilePath -> (Value -> m a) -> m a
 withJsonFile path f = do
   fileContents <- liftIO $ ByteString.readFile path
